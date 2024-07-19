86f6198f925b45a73f8511a546578d257a1152f9
Christian Albertini <cja12classic@hotmail.com>
2024-07-18 21:27:27 -0400

    Edited file

--- pull_api.py
+++ pull_api.py
@@ -12,9 +12,9 @@ query_url="?maxYear=2024&minYear=2000"
 url=base_url+volcano+query_url
 url2=base_url+earthquake+query_url
 url3=base_url+tsunami+query_url
-response=requests.get(url)
-data=response.json()
-print(json.dumps(data, indent=4, sort_keys=True))
+# response=requests.get(url)
+# data=response.json()
+# print(json.dumps(data, indent=4, sort_keys=True))
 
 df = pd.read_csv(url)
 df2 = pd.read_csv(url2)
