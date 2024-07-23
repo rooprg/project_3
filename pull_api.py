fd218889e7ead5b65d1d6258ff254688de4d9334
rooprg <richardroop.pmp@icloud.com>
2024-07-22 21:35:59 -0400

    Data collection 22_jul-2024

--- pull_api.py
+++ pull_api.py
@@ -1,24 +1,34 @@
+# Import dependencies
 import requests
 import json
 import pandas as pd
 
+# Set base url
 base_url = "https://www.ngdc.noaa.gov/hazel"
 
+# Develop queries text
 volcano = "/hazard-service/api/v1/volcanoes"
 earthquake="/hazard-service/api/v1/earthquakes"
 tsunami="/hazard-service/api/v1/tsunamis/events"
 query_url="?maxYear=2024&minYear=2000"
 
+# Format queries
 url=base_url+volcano+query_url
 url2=base_url+earthquake+query_url
 url3=base_url+tsunami+query_url
-# response=requests.get(url)
-# data=response.json()
-# print(json.dumps(data, indent=4, sort_keys=True))
+response=requests.get(url)
+volcano=response.json()
+response2=requests.get(url2)
+earthquake=response2.json()
+response3=requests.get(url3)
+tsunami=response3.json()
 
-df = pd.read_csv(url)
-df2 = pd.read_csv(url2)
-df3 = pd.read_csv(url3)
-df.to_csv("volcano.csv", index=False)
-df2.to_csv("earthquake.csv", index=False)
-df3.to_csv("tsunami.csv", index=False)
+# Generating jsons
+with open("volcano.json", mode="w", encoding="utf-8") as write_file:
+    json.dump(volcano, write_file)
+
+with open("earthquake.json", mode="w", encoding="utf-8") as write_file:
+    json.dump(earthquake, write_file)
+
+with open("tsunami.json", mode="w", encoding="utf-8") as write_file:
+    json.dump(tsunami, write_file)
