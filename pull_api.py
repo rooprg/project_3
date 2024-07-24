151f565dd4b5838a65b127eb0f0705409cbd9a21
rooprg <richardroop.pmp@icloud.com>
2024-07-24 19:50:08 -0400

    Hooray we did it

--- pull_api.py
+++ pull_api.py
@@ -12,23 +12,43 @@ earthquake="/hazard-service/api/v1/earthquakes"
 tsunami="/hazard-service/api/v1/tsunamis/events"
 query_url="?maxYear=2024&minYear=2000"
 
-# Format queries
+# Create volcano json
+
 url=base_url+volcano+query_url
-url2=base_url+earthquake+query_url
-url3=base_url+tsunami+query_url
 response=requests.get(url)
 volcano=response.json()
-response2=requests.get(url2)
-earthquake=response2.json()
-response3=requests.get(url3)
-tsunami=response3.json()
+
+# Create for loop for earthquakes
+list = [1,2,3,4,5,6,7]
+query_url2 = "?maxYear=2024&minYear=2000&page={}"
+url2 = base_url+earthquake+query_url2
+earthquake_items = []
+
+for i in list:
+    earthquake_url2 = url2.format(i)
+    response2=requests.get(earthquake_url2)
+    earthquake=response2.json()
+    earthquake_items += earthquake["items"]
+
+# Create for loop for tsunamis
+list2 = [1,2]
+query_url3 = "?maxYear=2024&minYear=2000&page={}"
+url3 = base_url+tsunami+query_url3
+tsunami_items = []
+
+for i in list2:
+    tsunami_url3 = url3.format(i)
+    response3=requests.get(tsunami_url3)
+    tsunami=response3.json()
+    tsunami_items += tsunami["items"]
+
 
 # Generating jsons
 with open("volcano.json", mode="w", encoding="utf-8") as write_file:
     json.dump(volcano, write_file)
 
 with open("earthquake.json", mode="w", encoding="utf-8") as write_file:
-    json.dump(earthquake, write_file)
+    json.dump(earthquake_items, write_file)
 
 with open("tsunami.json", mode="w", encoding="utf-8") as write_file:
-    json.dump(tsunami, write_file)
+    json.dump(tsunami_items, write_file)
