5ab00039ac30bb2aa831b9f6711315664fcf28c5
Christian Albertini <cja12classic@hotmail.com>
2024-07-27 01:33:05 -0400

    Finishing Christian's code

--- logic_christian.js
+++ logic_christian.js
@@ -4,21 +4,88 @@ for (let i=2000; i<2025; i++) {
     year.push(i);
 }
 
-function getChartData(url){
-    $.ajax({
-        url: url,
-        async: false,
-        dataType: "json",
-        success: function(result) {
-            let data1=result.map(x=>x.deathsTotal)
-            return(data1);
+const footer = (tooltipItems)=>{
+    let total=0;
+    tooltipItems.forEach(function(tooltipItem){
+        total += tooltipItem.parsed.y;
+    });
+    return 'Total: '+total;
+};
+
+function renderChart(data1, data2, data3, labels){
+    var ctx = document.getElementById("myChart").getContext("2d");
+    var myChart = new Chart(ctx, {
+        type: "bar",
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Earthquakes',
+                barThickness:10,
+                backgroundColor: 'green',
+                data: data1[0].map(x=>x.deathsTotal)
+            },
+            {
+                label: 'Volcano',
+                barThickness:10,
+                backgroundColor: 'red',
+                data: data2[0].map(x=>x.deathsTotal)
+            },
+            {
+                label: 'Tsunami',
+                backgroundColor: 'blue',
+                barThickness:10,
+                data: data3[0].map(x=>x.deathsTotal)
+            }]
+        },
+        options: {
+            interaction: {
+                intersect: false,
+                mode: 'index',
+            },
+            plugins: {
+                tooltip: {
+                    callbacks: {
+                        footer: footer,
+                    }
+                },
+            },
+            responsive: true,
+            scales:{
+                x:{
+                    stacked: true
+                },
+                y:{
+                    stacked: true,
+                    max: 1500
+                }
+                
+            }
         }
     });
 }
+//console.log(getChartData("Datasets/cleaned_earthquake.json"))
+$.when($.ajax("Datasets/cleaned_earthquake.json"), $.ajax("Datasets/cleaned_volcanos.json"), $.ajax("Datasets/cleaned_tsunami.json")).done(function(response1, response2, response3){
+    renderChart(response1, response2, response3, year)
+    //console.log(response1[0].map(x=>x.deathsTotal))
+    
+})
 
-function remap(data){
+//Coding attempts
+// function getChartData(url){
+//     $.ajax({
+//         url: url,
+//         async: false,
+//         dataType: "json",
+//         success: function(result) {
+//             let data1=result.map(x=>x.deathsTotal)
+//             return(data1);
+//         }
+//     });
+// }
 
-}
+// function remap(data){
+
+// }
 // $.when(
 //     $.ajax({
 //         url: url,
@@ -54,49 +121,4 @@ function remap(data){
 //         });
 //     });
 // }
-
-
-function renderChart(data1, data2, data3, labels){
-    var ctx = document.getElementById("myChart").getContext("2d");
-    var myChart = new Chart(ctx, {
-        type: "bar",
-        data: {
-            labels: labels,
-            datasets: [{
-                label: 'Earthquakes',
-                barThickness:2,
-                backgroundColor: 'red',
-                data: data1[0].map(x=>x.deathsTotal)
-            },
-            {
-                label: 'Volcano',
-                barThickness:2,
-                backgroundColor: 'yellow',
-                data: data2[0].map(x=>x.deathsTotal)
-            },
-            {
-                label: 'Tsunami',
-                backgroundColor: 'blue',
-                barThickness:2,
-                data: data3[0].map(x=>x.deathsTotal)
-            }]
-        },
-        options: {
-            scales:{
-                x:{
-                    stacked: true
-                },
-                y:{
-                    stacked: true
-                }
-            }
-        }
-    });
-}
-//console.log(getChartData("Datasets/cleaned_earthquake.json"))
-$.when($.ajax("Datasets/cleaned_earthquake.json"), $.ajax("Datasets/cleaned_volcano.json"), $.ajax("Datasets/cleaned_tsunami.json")).done(function(response1, response2, response3){
-    renderChart(response1, response2, response3, year)
-    console.log(response1[0].map(x=>x.deathsTotal))
-    
-})
 
